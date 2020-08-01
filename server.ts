ea335fcc3d9a150b737f0fb5a7ae0c91da7143df
jonathanlee06 <43515846+jonathanlee06@users.noreply.github.com>
2020-08-01 12:14:43 +0800

    Edit Port

--- server.ts
+++ server.ts
@@ -1,8 +1,13 @@
 import { listenAndServe } from "https://deno.land/std/http/server.ts";
 import { acceptWebSocket, acceptable } from "https://deno.land/std/ws/mod.ts";
+import { parse } from "https://deno.land/std@0.59.0/flags/mod.ts";
 import { chat } from "./chat.ts";
 
-listenAndServe({ port: 3000 }, async (req) => {
+const DEFAULT_PORT = 3000;
+const argPort = parse(Deno.args).port;
+const port = argPort ? parseInt(argPort) : DEFAULT_PORT
+
+listenAndServe({ port: port }, async (req) => {
     if (req.method === "GET" && req.url === "/") {
         req.respond({
             status: 200,
